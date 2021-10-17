f25ecf77205a2ef63040a7315d54c9b191cd0e34
a b <a@b.com>
2021-10-18 12:16:46 +1300

    - Upgraded to Python 3.9 and update dependencies. - Added more users.

--- README.rst
+++ README.rst
@@ -1,6 +1,6 @@
 Run Countdowner
 ***************
-A Python 3.8+ script to collect some product sales information from Countdown grocery store.
+A Python 3.9+ script to collect some product sales information from Countdown grocery store.
 
 
 Authors
@@ -10,6 +10,12 @@ Authors
 
 Changes
 =======
+
+1.1.0, 2021-10-18
+-----------------
+- Upgraded to Python 3.9 and update dependencies.
+- Added more users.
+
 
 1.0.0, 2020-12-10
 -----------------
--- find_sales.py
+++ find_sales.py
@@ -10,12 +10,17 @@ de.load_dotenv()
 U = os.getenv("GMAIL_USERNAME")
 P = os.getenv("GMAIL_PASSWORD")
 
+if not U or not P:
+    raise ValueError("Could not find GMail credentials")
+
 # Run pipeline
 config = [
     ["ibatatas@runbox.com", "https://docs.google.com/spreadsheets/d/1Cu8wEckdfGPtkN9zj5H8O_4ZUrQyrXBZ8Um-K2BSlnc/edit?usp=sharing"],
     ["jenklosser@gmail.com", "https://docs.google.com/spreadsheets/d/1IXh32XgrUq1O_xWq5SqXZlhwGmoK2tChwFqP2AEXJUo/edit?usp=sharing"],
     ["helena.teichrib@gmail.com", "https://docs.google.com/spreadsheets/d/1v2ZmJrSwSok0dTeVV6KBksAVa_eTTFE2YDQheXAYZT4/edit?usp=sharing"],
     ["dobozban@gmail.com", "https://docs.google.com/spreadsheets/d/1KBYj80WRxFSojyO6lKLswxrshNIJea_aro3lIjnVnqU/edit?usp=sharing"],
+    ["jesse.p.prendergast@gmail.com", "https://docs.google.com/spreadsheets/d/10X11TILgnFvOyEs6K1rntTOvE2w3WmsxLKWsk5ALTjM/edit?usp=sharing"],
+    ["cstewart000@gmail.com", "https://docs.google.com/spreadsheets/d/16oAl4UZNHN9wSOK_n0y_yBv7FVZ5y3IlkF9gygwoH7E/edit?usp=sharing"],
 ]
 for email, url in config:
     print(f"Working on watchlist for {email}...")
@@ -26,3 +31,4 @@ for email, url in config:
         gmail_password=P,
         sales_only=True,
     )
+
